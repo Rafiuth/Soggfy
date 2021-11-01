ee16f6f63f4ccc1a095afcda0887b2ded744e897
Rafiuth <53208252+Rafiuth@users.noreply.github.com>
2021-10-31 23:40:42 -0300

    Fix DownloadFFmpeg script
    - Add UseBasicParsing flag to avoid IE dependency
    - Add support for Win32

--- SpotifyOggDumper/Data/DownloadFFmpeg.ps1
+++ SpotifyOggDumper/Data/DownloadFFmpeg.ps1
@@ -1,21 +1,23 @@
-if (![Environment]::Is64BitOperatingSystem) {
-    Write-Host "This script only supports 64-bit OS."
-    pause
-    return
+if ([Environment]::Is64BitOperatingSystem) {
+    $repoUrl = "https://api.github.com/repos/BtbN/FFmpeg-Builds/releases/latest"
+    $platform = "win64"
+} else {
+    $repoUrl = "https://api.github.com/repos/sudo-nautilus/FFmpeg-Builds-Win32/releases/latest"
+    $platform = "win32"
 }
-
-$release = Invoke-WebRequest "https://api.github.com/repos/BtbN/FFmpeg-Builds/releases/latest" | ConvertFrom-Json
+$release = Invoke-WebRequest $repoUrl -UseBasicParsing | ConvertFrom-Json
 
 foreach ($asset in $release.assets) {
-    if ($asset.name -cmatch 'ffmpeg-n.+win64-gpl-shared') {
+    if ($asset.name -cmatch "ffmpeg-n.+$platform-gpl-shared") {
         Write-Host "Downloading $($asset.name)..."
 
-        # Invoke-WebRequest is too slow, had to use WebClient manually. 
-        # https://stackoverflow.com/questions/28682642/powershell-why-is-using-invoke-webrequest-much-slower-than-a-browser-download
+        # Invoke-WebRequest is too slow - https://stackoverflow.com/questions/28682642/powershell-why-is-using-invoke-webrequest-much-slower-than-a-browser-download
         (New-Object Net.WebClient).DownloadFile($asset.browser_download_url, $asset.name)
-
+        
         # Remove previous installation
-        Remove-Item -Path "./ffmpeg/" -Recurse -Force
+        if (Test-Path "./ffmpeg/") {
+            Remove-Item -Path "./ffmpeg/" -Recurse -Force
+        }
         New-Item -ItemType Directory -Path "./ffmpeg/" -Force
         
         # Extract bin/ folder to ffmpeg/
@@ -35,5 +37,5 @@ foreach ($asset in $release.assets) {
     }
 }
 
-Write-Host "Could not find any matching ffmpeg builds from BtbN"
+Write-Host "Could not find any matching ffmpeg builds, please open an issue or check https://ffmpeg.org/download.html"
 pause
