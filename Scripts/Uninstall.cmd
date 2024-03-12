cbe818d8e1f956b7d24c1d677beaf416081e917f
Tetrapak <112382253+Tetrapak0@users.noreply.github.com>
2024-03-12 14:05:02 +0100

    Fix Uninstall.cmd

--- Scripts/Uninstall.cmd
+++ Scripts/Uninstall.cmd
@@ -1,5 +1,5 @@
-del %appdata%\Spotify\dpapi.dll
-del %appdata%\Spotify\SoggfyUIC.js
-rmdir /q /s %localappdata%\Soggfy
+del "%appdata%\Spotify\dpapi.dll"
+del "%appdata%\Spotify\SoggfyUIC.js"
+rmdir /q /s "%localappdata%\Soggfy"
 
 pause
